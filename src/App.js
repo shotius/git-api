411967cc46778af9801826f95f5f1c933eab68b6
shotius <sh.archemashvili@gmail.com>
2020-12-24 14:12:33 +0400

    created route basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route
+} from "react-router-dom";
+//import {  } from 'react-router'
+import {useState} from 'react'
 
-function App() {
+export default function App() {
+  const [isRedirect, setIsRedirect] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    setIsRedirect(true)
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        
+        {isRedirect ? <Redirect to="/about"/> : <Redirect to="/" />}
+        
+        <Switch>
+          <Route exact path="/about">
+            <About setIsRedirect={setIsRedirect} isRedirect={isRedirect}/>
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About({isRedirect, setIsRedirect}) {
+  const handleBack = (e) => {
+    e.preventDefault();
+    console.log("clicked back")
+    setIsRedirect(false)
+  }
+  console.log(isRedirect) 
+  return (
+    <div>
+    <h2>I cant believe I am on 'About' page</h2>
+    <form onSubmit={handleBack}>
+      <button type="submit">back to main</button>
+    </form>
+    </div>
+
+  );
+}
